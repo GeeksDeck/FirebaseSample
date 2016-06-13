60cb071abe13d880d7ee3019e478ce492ee4be15
Paweł Cyroń <paul.cyron1@gmail.com>
2016-06-13 08:48:09 +0200

    init commit with firebase support added

--- FirebaseSample/app/src/main/java/com/geeksdeck/firebasesample/MainActivity.java
+++ FirebaseSample/app/src/main/java/com/geeksdeck/firebasesample/MainActivity.java
@@ -1,13 +1,119 @@
 package com.geeksdeck.firebasesample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+    private static final int SIGN_IN_REQUEST_CODE = 8765; // random, unique
+
+    SignInButton signInButton;
+    Button signOutButton;
+    TextView welcomeSign;
+    GoogleApiClient googleApiClient;
+    GoogleSignInAccount account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        //Init Google Api objects
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail() // request an user email for our credentials
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    /**
+     * Init everything from view
+     */
+    private void initView() {
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signOutButton = (Button) findViewById(R.id.sign_out_button);
+        welcomeSign = (TextView) findViewById(R.id.welcome_sign);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN_REQUEST_CODE);
+            }
+        });
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            signOutButton.setVisibility(View.GONE);
+                            welcomeSign.setText("Thank You,\ncome again");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection to GoogleClientApi failed, please try again",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //if request code equals our intent request code
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSingInResult(result);
+        }
+    }
+
+    private void handleSingInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            signOutButton.setVisibility(View.VISIBLE);
+            account = result.getSignInAccount();
+            welcomeSign.setText("Welcome, \n" + account.getDisplayName());
+        } else {
+            signOutButton.setVisibility(View.GONE);
+
+            // Let user know that signing process failed
+            Toast.makeText(this, "Failure to sign in", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText(buttonText);
+                return;
+            }
+        }
     }
 }
